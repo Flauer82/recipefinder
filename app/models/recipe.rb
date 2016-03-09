d11e62e09b2d9bc49e060790eae9c2af1563438d
Lorenzo Fiorentino <lorenzo.fiorentino@gmail.com>
2016-03-09 19:45:55 +0100

    All rspec passed!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,7 @@ class Recipe
   #default_options.update(verify: false) # Turn off SSL verification
   key_value = ENV['FOOD2FORK_KEY']
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  #hostport = 'food2fork.com'
   base_uri "http://#{hostport}/api"
   default_params key: key_value
   format :json
